df0a3a4f6aabed796aa402b6ae5c4e32671e10d6
yanma <lingyuyan123@163.com>
2017-06-27 09:33:02 +0800

    remove MY_SELL project

--- MY_SELL/Pods/Target Support Files/Pods-MY_SELL/Pods-MY_SELL-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MY_SELL : NSObject
-@end
-@implementation PodsDummy_Pods_MY_SELL
-@end
--- MY_SELL/Pods/Target Support Files/Pods-MY_SELLUITests/Pods-MY_SELLUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MY_SELLUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MY_SELLUITests
-@end
